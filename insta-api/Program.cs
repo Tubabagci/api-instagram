2b2445a3d6d63d8776bd86fed11ebfbf51583eb0
Tubabagci <57172097+Tubabagci@users.noreply.github.com>
2022-08-15 17:51:27 +0300

    Proje dosyası ekle.

---
+++ insta-api/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace insta_api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
