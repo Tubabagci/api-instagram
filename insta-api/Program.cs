2dba6f56377036fc11ea1eb4dc159267879bf582
Tubabagci <57172097+Tubabagci@users.noreply.github.com>
2022-08-15 17:58:55 +0300

    deneme

--- insta-api/Program.cs
+++ insta-api/Program.cs
@@ -7,6 +7,13 @@ namespace insta_api
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
         }
     }
 }
